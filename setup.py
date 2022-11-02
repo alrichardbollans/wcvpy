e377e8475a17c44b12848653553d030333c46405
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-11-02 10:03:19 +0000

    Rmv setup from dev

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 # To install run pip install -e PATH_TO_PACKAGE
 
-setup(
-    name='automatchnames',
-    version='0.0.1',
-    packages=['automatchnames','taxa_lists'],
-    url='https://github.com/alrichardbollans/automatchnames',
-    license='GNU v.3',
-    author='Adam Richard-Bollans',
-    author_email='adamrb@protonmail.com',
-    description='A package for automating name matching of scientific names',
-    long_description=open('readme.md',encoding="utf8").read()
-)
+# setup(
+#     name='automatchnames',
+#     version='0.0.1',
+#     packages=['automatchnames','taxa_lists'],
+#     url='https://github.com/alrichardbollans/automatchnames',
+#     license='GNU v.3',
+#     author='Adam Richard-Bollans',
+#     author_email='adamrb@protonmail.com',
+#     description='A package for automating name matching of scientific names',
+#     long_description=open('readme.md',encoding="utf8").read()
+# )

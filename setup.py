3b6bfb6e4673d2de993dabd12551b28b0d35b752
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-11-18 16:03:52 +0000

    Fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='automatchnames',
-    version='0.0.2',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         'pandas==1.4.1',

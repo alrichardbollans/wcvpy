701de56e7890c2f2e7fbe1087b4312879f4b8b43
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-11-18 16:04:30 +0000

    Fix version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='automatchnames',
-    version='0.0.1',
+    version='0.1',
     packages=['automatchnames','taxa_lists'],
     url='https://github.com/alrichardbollans/automatchnames',
     license='GNU v.3',

7672c960d62089f9f422ce3ca1d1e5dec7db366c
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2023-02-17 10:45:54 +0000

    Dev setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='automatchnames',
-    version='1.0',
+    version='DEVX',
     packages=find_packages(),
     install_requires=[
         'pandas==1.4.1',

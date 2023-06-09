d87bb39f13bdab3d3ac880188283d023554dba2b
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2023-06-09 12:06:57 +0100

    Use OpenRefine, include author info in outputs, other minor improvements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='automatchnames',
-    version='1.1.1',
+    version='1.2.0',
     packages=find_packages(),
     install_requires=[
         'pandas==2.0.1',

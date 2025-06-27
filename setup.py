50a0991d994c4f0899326c78d67c0f1e1b0c9c71
Adam Richard-Bollans <38588335+alrichardbollans@users.noreply.github.com>
2025-06-27 12:25:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wcvpy',
-    version='1.3.2',
+    version='1.3.3',
     packages=find_packages(),
     package_data={"wcvpy": ["wcvp_download/inputs/*", "wcvp_download/inputs/wgsrpd-master/level3/*"]},
     install_requires=[
